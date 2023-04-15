b00b84ff75fa97391c695aab7bcad669549e1d5f
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-15 15:45:39 -0500

    add unique ID npm and outline get route file for post and delete

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
-// include file system to read, write, append
 const fs = require('fs');
-
-// JSON
 const notes = require('../db/db.json');
+var uniqueID = require('uniqid'); 
 
 // GET /api/notes (return db.json)
 router.get('/notes', (req, res) => {
@@ -13,11 +10,13 @@ router.get('/notes', (req, res) => {
 });
 
 // POST /api/notes (return new note, add to db.json, return to client, give note unique ID when saved)
+router.post('/notes', (req, res) => {
 
-// create unique & random note IDs using 'random' npm
-// const random = require('random');
-// const uniqueID = random.int(1000, 5000);
+});
 
-// DELETE /api/notes/:id (bonus)
+// DELETE /api/notes/:id 
+router.delete('/notes/:id', (req, res) => {
+
+});
 
 module.exports = router;
