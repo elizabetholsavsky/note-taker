814ac5808146139ee1af31cb325ace390e563179
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-15 14:10:34 -0500

    add randon npm for unique note ID, import and create const

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+// create unique & random note IDs using random npm
+import random from 'random';
+const uniqueID = random.int(1000, 5000);
+
 // GET /api/notes (return db.json)
 
 // POST /api/notes (return new note, add to db.json, return to client)***
