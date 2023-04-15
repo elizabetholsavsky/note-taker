1c7c2f9d4fb98cee0490ce35cf10db9875f8f073
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-15 15:15:22 -0500

    get db.json with /api/notes, run in insomnia

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,23 @@
-// API ROUTES
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 
-// create unique & random note IDs using random npm
-import random from 'random';
-const uniqueID = random.int(1000, 5000);
+// include file system to read, write, append
+const fs = require('fs');
+
+// JSON
+const notes = require('../db/db.json');
 
 // GET /api/notes (return db.json)
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
 
-// POST /api/notes (return new note, add to db.json, return to client)***
-    // ***give note unique ID when saved
+// POST /api/notes (return new note, add to db.json, return to client, give note unique ID when saved)
 
-// DELETE /api/noyes/:id (bonus)
+// create unique & random note IDs using 'random' npm
+// const random = require('random');
+// const uniqueID = random.int(1000, 5000);
+
+// DELETE /api/notes/:id (bonus)
 
 module.exports = router;
