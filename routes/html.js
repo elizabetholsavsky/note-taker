2f70cae25a4cf2e0b0659a1310ff94ea8c2f50fa
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-15 13:22:07 -0500

    implememnt separation of concerns, create route folder and js files, add comments, link to server

---
+++ routes/html.js
@@ -0,0 +1,9 @@
+// HTML ROUTES
+const express = require('express');
+const router = express.Router();
+
+// GET /notes (return notes.html)
+
+// GET * (return index.html)
+
+module.exports = router;
